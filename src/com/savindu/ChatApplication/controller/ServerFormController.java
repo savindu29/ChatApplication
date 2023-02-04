5f276eda2d6911e23d038ce679fce5eb7ac2e58f
savindu29 <1savindupanagoda@gmail.com>
2023-02-05 03:53:22 +0530

    init

---
+++ src/com/savindu/ChatApplication/controller/ServerFormController.java
@@ -0,0 +1,8 @@
+package com.savindu.ChatApplication.controller;
+
+import javafx.scene.input.MouseEvent;
+
+public class ServerFormController {
+    public void shutdownServerOnClick(MouseEvent mouseEvent) {
+    }
+}
