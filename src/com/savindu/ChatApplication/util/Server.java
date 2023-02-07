bd536f9bf02582fb20ea82e25589ce31639c181a
savindu29 <1savindupanagoda@gmail.com>
2023-02-07 20:03:13 +0530

    developed send message

--- src/com/savindu/ChatApplication/util/Server.java
+++ src/com/savindu/ChatApplication/util/Server.java
@@ -19,12 +19,14 @@ public class   Server {
     }
     public void startServer(VBox server_vBox){
         try{
-            socket = serverSocket.accept();
-            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            ClientHandler clientHandler = new ClientHandler(socket, server_vBox);
-            Thread thread = new Thread(clientHandler);
-            thread.start();
+            while(!ifShutdown) {
+                socket = serverSocket.accept();
+                this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                ClientHandler clientHandler = new ClientHandler(socket, server_vBox);
+                Thread thread = new Thread(clientHandler);
+                thread.start();
+            }
         }catch (IOException e){
             e.printStackTrace();
         }
